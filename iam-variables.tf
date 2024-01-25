7d93447a624f8ba183de8d06cb3a4e1fbef1b2fe
Rory Nguyen <rory.nguyen@oracle.com>
2024-01-25 08:58:45 -0800

    updated provider version

--- iam-variables.tf
+++ iam-variables.tf
@@ -2,3 +2,8 @@ variable "tenancy_ocid" {
   type        = string
   description = "the OCID of tenancy"
 }
+
+variable "mission_owner_workload_admin_group" {
+  type        = string
+  description = "Name of the Mission Owner Workload Admin user group"
+}
