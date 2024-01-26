c5abc4843e438e1aa785cad9b094fcc1a3b6c564
Rory Nguyen <rory.nguyen@oracle.com>
2024-01-26 09:20:57 -0800

    remove identity domain

--- iam-variables.tf
+++ iam-variables.tf
@@ -7,3 +7,29 @@ variable "mission_owner_workload_admin_group" {
   type        = string
   description = "Name of the Mission Owner Workload Admin user group"
 }
+
+variable "idcs_endpoint" {
+  type = string
+  description = "URL endpoint of the existing identity domain"
+}
+
+variable "identity_domain_name" {
+  type = string
+  description = "Identity domain display name"
+}
+
+variable "master_encryption_key_ocid" {
+  type = string
+  description = "OCID of the master encryption key"
+}
+
+variable "vdms_compartment_name" {
+  type = string
+  description = "Name of the VDMS compartment"
+}
+
+variable "resource_label" {
+  type = string
+  description = "Unique prefix to avoid naming conflict"
+}
+
--- iam.tf
+++ iam.tf
@@ -1,40 +1,18 @@
 locals {
-  identity_domain = {
-    domain_description        = "OCI SCCA Landing Zone Identity Domain"
-    domain_display_name       = "OCI-SCCA-LZ-Domain-${local.region_key[0]}-${var.mission_owner_key}"
-    domain_license_type       = "premium"
-    domain_is_hidden_on_login = false
-
-    group_names = [var.mission_owner_workload_admin_group]
+  workload_policy = {
+    name        = "OCI-SCCA-LZ-Workload-Policy"
+    description = "This account is required for the management of the Mission Application workloads."
+    statements = [
+      "Allow group ${var.identity_domain_name}/${var.mission_owner_workload_admin_group} to manage all-resources in compartment ${module.workload_compartment.compartment_name}",
+      "Allow group ${var.identity_domain_name}/${var.mission_owner_workload_admin_group} to use key-delegate in compartment ${var.vdms_compartment_name}-${local.region_key[0]}-${var.resource_label} where target.key.id = ${var.master_encryption_key_ocid}"
+    ]
   }
-}
-
-module "identity_domain" {
-  source                    = "./modules/identity-domain"
-  compartment_id            = var.home_compartment_id
-  domain_description        = local.identity_domain.domain_description
-  domain_display_name       = local.identity_domain.domain_display_name
-  domain_license_type       = local.identity_domain.domain_license_type
-  domain_is_hidden_on_login = local.identity_domain.domain_is_hidden_on_login
-  domain_home_region        = var.region
 }
 
 module "workload_admin_group" {
   source             = "./modules/identity-domains-groups"
   group_display_name = var.mission_owner_workload_admin_group
-  idcs_endpoint      = module.identity_domain.domain.url
-}
-
-
-locals {
-  workload_policy = {
-    name        = "OCI-SCCA-LZ-Workload-Policy"
-    description = "This account is required for the management of the Mission Application workloads."
-    statements = [
-      "Allow group ${local.identity_domain.domain_display_name}/${var.mission_owner_workload_admin_group} to manage all-resources in compartment ${module.workload_compartment.compartment_name}",
-      "Allow group ${local.identity_domain.domain_display_name}/${var.mission_owner_workload_admin_group} to use key-delegate in compartment ${var.vdms_compartment_name}-${local.region_key[0]}-${var.resource_label} where target.key.id = ${var.master_encryption_key_ocid}"
-    ]
-  }
+  idcs_endpoint      = var.idcs_endpoint
 }
 
 module "workload_policy" {
