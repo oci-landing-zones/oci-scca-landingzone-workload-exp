e7a82314b02fea5d0ead4fccefe9acb12c3ce37c
Rory Nguyen <rory.nguyen@oracle.com>
2024-01-29 14:33:06 -0800

    Remove New Identity Domain (#4)
    * remove identity domain
    
    * added resource label to policy and region variable
    
    * formatting
    
    * updated vdms name
    
    * remove resource label var

--- iam-variables.tf
+++ iam-variables.tf
@@ -3,7 +3,32 @@ variable "tenancy_ocid" {
   description = "the OCID of tenancy"
 }
 
+variable "region" {
+  type        = string
+  description = "Region the resources will be deployed in"
+}
+
 variable "mission_owner_workload_admin_group" {
   type        = string
   description = "Name of the Mission Owner Workload Admin user group"
+}
+
+variable "idcs_endpoint" {
+  type        = string
+  description = "URL endpoint of the existing identity domain"
+}
+
+variable "identity_domain_name" {
+  type        = string
+  description = "Identity domain display name"
+}
+
+variable "master_encryption_key_ocid" {
+  type        = string
+  description = "OCID of the master encryption key"
+}
+
+variable "vdms_compartment_name" {
+  type        = string
+  description = "Full name of the VDMS compartment, including region key"
 }
