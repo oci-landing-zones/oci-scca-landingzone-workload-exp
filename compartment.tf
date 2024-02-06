86bef04dc5bfb75315988aeb4108fb048e03414e
Yupei Yang <yupei.yang@oracle.com>
2024-02-06 10:33:22 -0800

    add license for each file

--- compartment.tf
+++ compartment.tf
@@ -1,3 +1,8 @@
+# ###################################################################################################### #
+# Copyright (c) 2023 Oracle and/or its affiliates, All rights reserved.                                  #
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl. #
+# ###################################################################################################### #
+
 locals {
   workload_compartment = {
     name        = "${var.workload_compartment_name}-${local.region_key[0]}-${var.mission_owner_key}"
